90de1c514f4d2e242f6e869aa0f330044d6438fa
Jack Ball <ball.jack2376@gmail.com>
2023-08-18 10:47:28 -0600

    alnmost done

--- app.js
+++ app.js
@@ -1,70 +1,105 @@
-import express from 'express';
-import session from 'express-session';
-import lodash from 'lodash';
-import morgan from 'morgan';
-import nunjucks from 'nunjucks';
-import ViteExpress from 'vite-express';
+import express from "express";
+import session from "express-session";
+import lodash from "lodash";
+import morgan from "morgan";
+import nunjucks from "nunjucks";
+import ViteExpress from "vite-express";
 
 const app = express();
-const port = '8000';
+const port = "8000";
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+app.use(express.static("public"));
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
 const MOST_LIKED_FOSSILS = {
   aust: {
-    img: '/img/australopith.png',
-    name: 'Australopithecus',
+    img: "/img/australopith.png",
+    name: "Australopithecus",
     num_likes: 584,
   },
   quetz: {
-    img: '/img/quetzal_torso.png',
-    name: 'Quetzal',
+    img: "/img/quetzal_torso.png",
+    name: "Quetzal",
     num_likes: 587,
   },
   steg: {
-    img: '/img/stego_skull.png',
-    name: 'Stegosaurus',
+    img: "/img/stego_skull.png",
+    name: "Stegosaurus",
     num_likes: 598,
   },
   trex: {
-    img: '/img/trex_skull.png',
-    name: 'Tyrannosaurus Rex',
+    img: "/img/trex_skull.png",
+    name: "Tyrannosaurus Rex",
     num_likes: 601,
   },
 };
 
 const OTHER_FOSSILS = [
   {
-    img: '/img/ammonite.png',
-    name: 'Ammonite',
+    img: "/img/ammonite.png",
+    name: "Ammonite",
   },
   {
-    img: '/img/mammoth_skull.png',
-    name: 'Mammoth',
+    img: "/img/mammoth_skull.png",
+    name: "Mammoth",
   },
   {
-    img: '/img/ophthalmo_skull.png',
-    name: 'Opthalmosaurus',
+    img: "/img/ophthalmo_skull.png",
+    name: "Opthalmosaurus",
   },
   {
-    img: '/img/tricera_skull.png',
-    name: 'Triceratops',
+    img: "/img/tricera_skull.png",
+    name: "Triceratops",
   },
 ];
 
-// TODO: Replace this comment with your code
+app.get("/", (req, res) => {
+  const { name } = req.session;
+  if (name) {
+    res.redirect("/top-fossils");
+  } else {
+    res.render("homepage.html.njk");
+  }
+});
 
-app.get('/random-fossil.json', (req, res) => {
+app.get("/get-name", (req, res) => {
+  const { name } = req.query;
+  req.session.name = name;
+  res.redirect("/top-fossils");
+});
+
+app.get("/top-fossils", (req, res) => {
+  const { name } = req.session;
+  if (req.session.name) {
+    res.render("top-fossils.html.njk", {
+      MOST_LIKED_FOSSILS,
+      name: name,
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/random-fossil.json", (req, res) => {
   const randomFossil = lodash.sample(OTHER_FOSSILS);
   res.json(randomFossil);
+});
+
+app.post("/like-fossil", (req, res) => {
+  const { name } = req.session;
+  const { liked_fossils } = req.body;
+  let likes = MOST_LIKED_FOSSILS[liked_fossils].num_likes;
+  likes += 1;
+  res.render("thank-you.html.njk", { name: name });
 });
 
 ViteExpress.listen(app, port, () => {
